823d1433a644ed606608b7edfb94ff5c183b2a5d
Sam Saccone <samccone@gmail.com>
2013-06-01 16:34:44 -0400

    initial header timeline grid

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,16 @@
-$().ready(App.start({
+$().ready(init);
+
+function init() {
+  App.start({
     mixURL: 'mix.json'
-}));
+  });
+
+  a = new timelineGrid({
+    pps: 50,
+    seconds: 100,
+    fillStyle: "rgb(112, 118, 128)",
+    height: 6,
+    lineWidth: 2,
+    appendTo: document.querySelector('header .timeline')
+  });
+}
---
+++ public/js/timeline-grid.js
@@ -0,0 +1,87 @@
+(function() {
+  this.timelineGrid = (function() {
+    function timelineGrid(args) {
+      this.options = args;
+      if (this.options.seconds == null) {
+        throw "you must pass seconds";
+      }
+      if (this.options.pps == null) {
+        throw "you must pass pps";
+      }
+      this.retina = window.devicePixelRatio || 1;
+      this.options.horzPadding = 20 * this.retina;
+      this.options.vertPadding = 20 * this.retina;
+      this.options.appendTo = this.options.appendTo || document.body;
+      this.options.pps = this.options.pps * this.retina;
+      this.options.lineWidth = this.options.lineWidth || 4;
+      this.options.fontStyle = this.options.fontStyle || '15px Helvetica';
+      this.options.fillStyle = this.options.fillStyle || "black";
+      this.options.fontFillStyle = this.options.fontFillStyle || this.options.fillStyle;
+      this.options.height = (this.options.height * this.retina || 50 * this.retina) + this.options.vertPadding;
+      this.options.width = this.options.seconds * this.options.pps + this.options.horzPadding;
+      this.canvas = document.createElement('canvas');
+      this.canvas.setAttribute('width', this.options.width);
+      this.canvas.setAttribute('height', this.options.height);
+      this.canvas.style.width = "" + (this.options.width / this.retina) + "px";
+      this.canvas.style.height = "" + (this.options.height / this.retina) + "px";
+      this.ctx = this.canvas.getContext('2d');
+      this.ctx.fillStyle = this.options.fillStyle;
+      this.options.appendTo.appendChild(this.canvas);
+      this.draw();
+    }
+
+    timelineGrid.prototype.formatTime = function(second) {
+      var min, oneM, seconds, tenM;
+
+      min = ~~(second / 60);
+      tenM = ~~(min / 10);
+      oneM = ~~(min / 10 % 1 * 10);
+      seconds = ("" + (second % 60)).length === 1 ? "0" + second % 60 : second % 60;
+      return "" + tenM + oneM + ":" + seconds;
+    };
+
+    timelineGrid.prototype.changeDuration = function(duration) {
+      this.options.seconds = duration;
+      this.ctx.clearRect(0, 0, this.options.width, this.options.height);
+      this.options.width = this.options.seconds * this.options.pps + this.options.horzPadding;
+      this.canvas.setAttribute('width', this.options.width);
+      this.canvas.style.width = "" + (this.options.width / this.retina) + "px";
+      this.ctx = this.canvas.getContext('2d');
+      return this.draw();
+    };
+
+    timelineGrid.prototype.changePps = function(pps) {
+      this.options.pps = pps;
+      return this.changeDuration(this.options.seconds);
+    };
+
+    timelineGrid.prototype.draw = function() {
+      var h, i, metrics, o, timeStamp, x, y, _i, _ref;
+
+      o = this.options;
+      this.ctx.font = o.fontStyle;
+      for (i = _i = 0, _ref = o.seconds; 0.5 > 0 ? _i < _ref : _i > _ref; i = _i += 0.5) {
+        h = i % 1 ? (o.height - o.vertPadding) / 2 : o.height - o.vertPadding;
+        x = i * o.pps + o.lineWidth / 2 + o.horzPadding;
+        y = o.height - h + o.horzPadding;
+        if (!(i % 1)) {
+          timeStamp = this.formatTime(i + 1);
+          metrics = this.ctx.measureText(timeStamp);
+          if (o.fillStyle !== o.fontFillStyle) {
+            this.ctx.fillStyle = o.fontFillStyle;
+          }
+          this.ctx.fillText(this.formatTime(i + 1), x - metrics.width / 2 + o.lineWidth / 2, this.options.vertPadding / 2);
+        }
+        if (o.fillStyle !== o.fontFillStyle) {
+          this.ctx.fillStyle = o.fillStyle;
+        }
+        this.ctx.rect(x, o.height - h, o.lineWidth, h);
+      }
+      return this.ctx.fill();
+    };
+
+    return timelineGrid;
+
+  })();
+
+}).call(this);
