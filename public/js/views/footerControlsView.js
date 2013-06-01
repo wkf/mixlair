cde29b5a96eafd3f5039dcf62566fa4eaa05e43e
Kevin Ennis <kennis84@gmail.com>
2013-06-01 18:41:58 -0400

    commntz

--- public/js/views/footerControlsView.js
+++ public/js/views/footerControlsView.js
@@ -17,22 +17,27 @@ App.module('Views', function(Views, App, Backbone, Marionette, $, _) {
       'click .ffwd': 'ffwd',
       'click .end': 'end',
     },
+    // jump to 0
     beginning: function(){
       mix.set('position', 0);
     },
+    // jump back 10s
     rewind: function(){
       var pos = mix.get('position');
       mix.set('position', Math.max(0, pos - 10));
     },
+    // jump ahead 10s
     ffwd: function(){
       var pos = mix.get('position')
         , max = mix.get('maxTime');
       mix.set('position', Math.min(max, pos + 10));
     },
+    // jump to the end
     end: function(){
       var max = mix.get('maxTime');
       mix.pause().set('position', max);
     },
+    // play or pause
     playMix: function( e ){
       if ( mix.get('playing') ){
         mix.pause();
