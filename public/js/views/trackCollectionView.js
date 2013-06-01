e61de599bdc7dffc3b9db7190e21402ffc52dcdc
Sam Saccone <samccone@gmail.com>
2013-06-01 19:26:00 -0400

    wip of tracks progress

--- public/js/views/trackCollectionView.js
+++ public/js/views/trackCollectionView.js
@@ -1,7 +1,11 @@
 App.module('Views', function(Views, App, Backbone, Marionette, $, _) {
   Views.TrackCollection = Backbone.Marionette.CollectionView.extend({
     tagName: "ul",
-
+    initialize: function() {
+      this.on('itemView:trackClicked', function() {
+        console.log("d");
+      });
+    },
     getItemView: function() {
       return App.Views.TrackItem;
     }
--- public/js/views/trackView.js
+++ public/js/views/trackView.js
@@ -2,6 +2,14 @@ App.module('Views', function(Views, App, Backbone, Marionette, $, _) {
   Views.TrackItem = Backbone.Marionette.ItemView.extend({
     template: "#track-template",
     tagName: "li",
-    className: "track"
+    className: "track",
+    events: {
+      "click": "trackClicked"
+    },
+    trackClicked: function() {
+      console.log(this);
+      this.$el.addClass('active');
+      this.trigger("trackClicked");
+    }
   });
 });
