73a8c4c1515758ef70a1d5b64db4f9a081e6427b
Jeff Escalante <hello@jenius.me>
2013-06-02 12:23:46 -0400

    fx value coming through nicely

--- public/js/views/effects/effectDetailView.js
+++ public/js/views/effects/effectDetailView.js
@@ -5,23 +5,22 @@ App.module('Views', function(Views, App, Backbone, Marionette, $, _) {
 
     tagName: 'li',
 
-    events: {
-      "drag .slider": "paramChange"
-    },
-
     ui: {
       "slider": ".slider"
     },
 
     onRender: function(){
       var self = this;
-      this.ui.slider.slider({ value: self.model.get('value') });
+      this.ui.slider.slider({
+        value: self.model.get('value'),
+        slide: _.bind(self.paramChange, this)
+      });
     },
 
-    paramChange: function(e) {
+    paramChange: function(e, ui) {
       console.log(this.model.get('name')) // name of the effect to apply
       console.log(this.model.get('track')) // track to apply the effect to
-      console.log(e); // drag event (value)
+      console.log(ui.value); // drag event (value)
     }
   });
 });
