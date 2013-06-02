ec556b56ff5101b5fc0419208132a8cbb2dc45f8
Kevin Ennis <kennis84@gmail.com>
2013-06-02 12:35:15 -0400

    fx ranges

--- public/js/util/fx.js
+++ public/js/util/fx.js
@@ -2,51 +2,134 @@ App.module('util', function(util, App, Backbone, Marionette, $, _) {
   util.fx = [
     { name: 'compressor',
       params: {
-        bypass: 1,
-        threshold: -20,
-        release: 250,
-        makeupGain: 1,
-        attack: 1,
-        ratio: 4,
-        knee: 5,
-        automakeup: false
+        bypass: {
+          range: [0, 1],
+          value: 1
+        },
+        threshold: {
+          range: [-60, 0], 
+          value: -20
+        },
+        release: {
+          range: [10, 2000],
+          value: 250
+        },
+        makeupGain: {
+          range: [1, 100],
+          value: 1
+        },
+        attack: {
+          range: [0, 1000],
+          value: 1
+        },
+        ratio: {
+          range: [1, 50],
+          value: 4
+        },
+        knee: {
+          range: [0, 40],
+          value: 5
+        },
+        automakeup: {
+          range: [0, 1],
+          value: false
+        }
       }
     },
     { name: 'chorus',
       params: {
-        bypass: 1,
-        feedback: 0.4,
-        delay: 0.0045,
-        depth: 0.7,
-        rate: 1.5
+        bypass: {
+          range: [0, 1],
+          value: 1
+        },
+        feedback: {
+          range: [0, 0.95],
+          value: 0.4
+        },
+        delay: {
+          range: [0, 1],
+          value: 0.0045
+        },
+        depth: {
+          range: [0, 1],
+          value: 0.7
+        },
+        rate: {
+          range: [0, 8],
+          value: 1.5
+        }
       }
     },
     { name: 'tremolo',
       params: {
-        bypass: 1,
-        intensity: 0.3,
-        stereoPhase: 0,
-        rate: 5
+        bypass: {
+          range: [0, 1],
+          value: 1,
+        }
+        intensity: {
+          range: [0, 1],
+          value: 0.3,
+        },
+        stereoPhase: {
+          range: [0, 180],
+          value: 0,
+        }
+        rate: {
+          range: [0.1, 11],
+          value: 5
+        }
       }
     },
     { name: 'delay',
       params: {
-        bypass: 1,
-        delayTime: 100,
-        feedback: 0.45,
-        cutoff: 20000,
-        wetLevel: 0.5,
-        dryLevel: 1
+        bypass: {
+          range: [0, 1],
+          value: 1
+        },
+        delayTime: {
+          range: [20, 1000],
+          value: 100
+        }
+        feedback: {
+          range: [0, 0.9],
+          value: 0.45
+        },
+        cutoff: {
+          range: [20, 20000],
+          value: 20000
+        },
+        wetLevel: {
+          range: [0, 1],
+          value: 0.5
+        },
+        dryLevel: {
+          range: [0, 1],
+          value: 1
+        }
       }
     },
     { name: 'reverb',
       params: {
-        bypass: 1,
-        highCut: 22050,
-        lowCut: 20,
-        dryLevel: 1,
-        wetLevel: 1,
-        impulse: 'impulses/plate.wav',
+        bypass: {
+          range: [0, 1],
+          value: 1
+        },
+        highCut: {
+          range: [20, 22050],
+          value: 22050
+        },
+        lowCut: {
+          range: [20, 22050],
+          value: 20
+        },
+        dryLevel: {
+          range: [0, 1],
+          value: 1
+        },
+        wetLevel: {
+          range: [0, 1],
+          value: 1
+        }
       }
     }
   ]
