96cc9583c894914e5bfdbdc04f9eaacc270ddcb0
Jeff Escalante <hello@jenius.me>
2013-06-01 21:36:40 -0400

    per track volume slider

--- public/js/views/trackView.js
+++ public/js/views/trackView.js
@@ -3,7 +3,9 @@ App.module('Views', function(Views, App, Backbone, Marionette, $, _) {
     template: "#track-template",
     tagName: "li",
     className: "track",
-
+    ui: {
+      gain: '.gain-control'
+    },
     events: {
       "click": "trackClicked"
     },
@@ -22,6 +24,20 @@ App.module('Views', function(Views, App, Backbone, Marionette, $, _) {
 
     trackClicked: function() {
       this.trigger("trackClicked");
+    },
+
+    onRender: function(){
+      this.activateGainSlider();
+    },
+
+    activateGainSlider: function(){
+      var self = this;
+      this.ui.gain.slider({
+        value: 90,
+        slide: function(e, ui){
+          $(ui.handle).prev('.alt').css({ width: ui.value + "%" })
+        }
+      });
     }
 
   });
