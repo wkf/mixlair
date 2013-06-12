7807f025bc0cbb0fbeb4a672e9f5f4d03da57ab2
Sam Saccone <samccone@gmail.com>
2013-06-11 21:26:38 -0400

    Add mix scaffold

---
+++ app/models/mix.rb
@@ -0,0 +1,3 @@
+class Mix < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130609190441) do
+ActiveRecord::Schema.define(:version => 20130612011213) do
+
+  create_table "mixes", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.decimal  "volume"
+    t.decimal  "tempo"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
